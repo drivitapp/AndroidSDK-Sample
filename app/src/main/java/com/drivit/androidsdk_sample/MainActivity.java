90485a635ca5618fe0ce6d5d10dc4736ed0756da
Carlos <carloswabreu@gmail.com>
2018-06-19 17:51:05 +0100

    Comment

--- app/src/main/java/com/drivit/androidsdk_sample/MainActivity.java
+++ app/src/main/java/com/drivit/androidsdk_sample/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+/*The main activity of the app*/
 public class MainActivity extends AppCompatActivity {
 
     @Override
